4d09ed2be4533a81eaaa2ddbfb8a0f5bdc5944fd
Alex Dunia <thealexdunia@gmail.com>
2023-03-24 21:05:57 +0100

    updates

--- js/about.7017da87.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunksbjm"]=self["webpackChunksbjm"]||[]).push([[443],{908:function(n,s,u){u.r(s),u.d(s,{default:function(){return f}});var t=u(252);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),r=[e];function c(n,s){return(0,t.wg)(),(0,t.iD)("div",a,r)}var i=u(744);const o={},b=(0,i.Z)(o,[["render",c]]);var f=b}}]);
-//# sourceMappingURL=about.7017da87.js.map
